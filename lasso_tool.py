af7cdd7a2edd294a2a8b6fe8073b619c0b46dc51
nishanta993 <nishanta.boro12@gmail.com>
2019-09-03 11:16:24 +0530

    Update lasso_tool.py

--- lasso_tool.py
+++ lasso_tool.py
@@ -8,7 +8,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def generate_theta(omega,n):
-
     theta=(omega*n)%(2*np.pi)
     return theta,n
 
@@ -21,26 +20,18 @@ def setlimit(i):
         plt.ylim(-10,20)
  
  def plot_theta(theta,x):
-
-    plt.plot(x,theta,'-o')
-    plt.show()
+        plt.plot(x,theta,'-o')
+        plt.show()
 
 def generate_train_sample(omega,n):
-  
-    
     np.random.seed(3)
     np.random.shuffle(n)
-
-    
     nts=n[0:10]                      
     nts.sort()
-    
-
     thta,n=generate_theta(omega,nts)     
     return thta,n
+
 def generate_test_samples(omega,n,nts):
-    
-    
     nst=np.setdiff1d(n,nts)
     np.random.seed(3)
     np.random.shuffle(nst)
@@ -48,6 +39,7 @@ def generate_test_samples(omega,n,nts):
     nst.sort()
     test,ntest=generate_theta(omega,nst)
     return test,ntest
+
 def lasso_regression(trains,phi):
     k=0
     swon=[]
@@ -59,8 +51,7 @@ def lasso_regression(trains,phi):
     max_iter=1000
     w=np.zeros(n_poly)
     residual=trains.copy()
-    lipscu_c=(np.power(phi,2)).sum(axis=0)  
-
+    lipscu_c=(np.power(phi,2)).sum(axis=0)
     while kross>0:
         for t in range(max_iter):
             for i in range(n_poly):
@@ -78,8 +69,8 @@ def lasso_regression(trains,phi):
             if w[i]==0:
                 kross=kross-1
         k=k+1
-    
     return w,weinter
+
 def plot_weights(weights):
     lamda=0.005*np.arange(k)
     for o in range(N):
@@ -99,7 +90,8 @@ def plot_testingdata(ntest,ytest,tests):
     plt.plot(ntest*Fs,tests,'-.',label='actual')
     plt.legend(loc='upper left')
     plt.show()
-def plot_trainingdata(ntrain,ytrain,trainsample):
+
+    def plot_trainingdata(ntrain,ytrain,trainsample):
     plt.title('N=10 ; D=%d training data'%(N-1))
     plt.xlabel('Values of n')
     plt.ylabel(r"$\theta(n)$")
